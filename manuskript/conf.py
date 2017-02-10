b0d0f1d3b0b62dccceec35f8e6a6bd63f4a925a2
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2017-02-10 08:49:05 +0100

    include license info in copyright

--- manuskript/conf.py
+++ manuskript/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Python für Naturwissenschaftler'
-copyright = u'2012-2016, Gert-Ludwig Ingold'
+copyright = u'2012-2017, Gert-Ludwig Ingold, Lizenz: CC BY 4.0 International'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
